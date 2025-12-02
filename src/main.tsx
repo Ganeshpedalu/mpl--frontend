5b762a97a8c5f455434177c7c7ee8aead78369a4
Ganeshpedalu <pedaluganesh2@gmail.com>
2025-12-02 13:29:15 +0530

    hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
-// Get base path from import.meta.env.BASE_URL (set by Vite)
-const basePath = import.meta.env.BASE_URL;
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={basePath}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
